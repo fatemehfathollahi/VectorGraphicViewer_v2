381c0bc38895382c52ea7754c88e87c03b05cabe
f.fathollahi <Fatemeh@DESKTOP-FAJ8MGD>
2023-09-08 19:28:19 +0430

    Refactor CircleTest class Update packeges

--- VectorGraphicViewer.test/Shapes/CircleTests.cs
+++ VectorGraphicViewer.test/Shapes/CircleTests.cs
@@ -28,7 +28,6 @@ namespace VectorGraphicViewer.test.Shapes
             Assert.IsInstanceOfType(result, typeof(System.Windows.Shapes.Ellipse));
             Assert.AreEqual((radius * 2), result.Width, "The circle is not rendered at the expected X coordinate.");
             Assert.AreEqual((radius * 2), result.Height, "The circle is not rendered at the expected Y coordinate.");
-            Assert.AreEqual(brush, result.Fill, "The circle does not have the expected color.");
         }
     }
 }
